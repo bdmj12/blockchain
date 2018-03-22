5a2761abd583fedc1b5e962f553f187461e7d9a2
bdmj12 <ben_jones-1995@hotmail.co.uk>
2018-03-22 14:44:11 +0000

    -

---
+++ Block.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Blockchain
+{
+    public class Block
+    {
+
+        public string hash;
+        public string previousHash;
+        private string data;
+        private long timeStamp; 
+        DateTime baseDate = new DateTime(1970, 1, 1);
+        public Block(string data, string previousHash)
+        {
+            this.data = data;
+            this.previousHash = previousHash;
+
+
+            TimeSpan diff = DateTime.Now - baseDate;
+            this.timeStamp = (long)(diff.TotalMilliseconds);
+
+                
+        }
+    }
+}
---
+++ Hash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Blockchain
+{
+
+    public class Hash
+    {
+        public static string GetHashSha256(string text)
+        // TAKES A STRING AND APPLIES SHA256 TO IT
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text); //NOT Encoding.Unicsode.GetBytes...
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+    }
+}
