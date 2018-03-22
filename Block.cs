63039004cb37d448a1511d01a7c08fa6125c1c05
bdmj12 <ben_jones-1995@hotmail.co.uk>
2018-03-22 15:41:18 +0000

    End of part 1 tutorial!
    https://medium.com/programmers-blockchain/create-simple-blockchain-java-tutorial-from-scratch-6eeed3cb03fa

--- Block.cs
+++ Block.cs
@@ -8,8 +8,11 @@ namespace Blockchain
         public string hash;
         public string previousHash;
         private string data;
-        private long timeStamp; 
+        private long timeStamp;
+        private int nonce;
+
         DateTime baseDate = new DateTime(1970, 1, 1);
+
         public Block(string data, string previousHash)
         {
             this.data = data;
@@ -19,7 +22,28 @@ namespace Blockchain
             TimeSpan diff = DateTime.Now - baseDate;
             this.timeStamp = (long)(diff.TotalMilliseconds);
 
-                
+            this.hash = CalculateHash();
+
+        }
+
+        public string CalculateHash()
+        {
+            string CalculatedHash = Hash.GetHashSha256(
+                previousHash + 
+                timeStamp.ToString() + 
+                nonce.ToString() +
+                data
+            );
+            return CalculatedHash;
+        }
+
+        public void MineBlock(int difficulty) {
+            string target = new string(new char[difficulty]).Replace("\0", "0");
+            while(!hash.Substring(0,difficulty).Equals(target)){
+                nonce++;
+                hash = CalculateHash();
+            }
+            Console.WriteLine("Block Mined! : " + hash);
         }
     }
 }
---
+++ MainClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blockchain
+{
+    public class MainClass
+    {
+
+        public static List<Block> blockchain = new List<Block>();
+        public static int difficulty = 4;
+
+        public static void Main(string[] args)
+
+        {
+            //add blocks to blockchain list:
+
+            blockchain.Add(new Block("Hi im the first block", "0"));
+            Console.WriteLine("Trying to Mine block 1... ");
+            blockchain[0].MineBlock(difficulty);
+
+            blockchain.Add(new Block("Yo im the second block", blockchain[blockchain.Count - 1].hash));
+            Console.WriteLine("Trying to Mine block 2... ");
+            blockchain[1].MineBlock(difficulty);
+
+            blockchain.Add(new Block("Hey im the third block", blockchain[blockchain.Count - 1].hash));
+            Console.WriteLine("Trying to Mine block 3... ");
+            blockchain[2].MineBlock(difficulty);
+
+            Console.WriteLine("\nBlockchain is Valid: " + IsChainValid());
+
+
+            // Json???
+
+        }
+
+
+
+
+        public static Boolean IsChainValid()
+        {
+            Block currentBlock;
+            Block previousBlock;
+            String hashTarget = new String(new char[difficulty]).Replace('\0', '0');
+
+            //loop through blockchain to check hashes:
+            for (int i = 1; i < blockchain.Count; i++)
+            {
+                currentBlock = blockchain[i];
+                previousBlock = blockchain[i - 1];
+                //compare registered hash and calculated hash:
+                if (!currentBlock.hash.Equals(currentBlock.CalculateHash()))
+                {
+                    Console.WriteLine("Current Hashes not equal");
+                    return false;
+                }
+                //compare previous hash and registered previous hash
+                if (!previousBlock.hash.Equals(currentBlock.previousHash))
+                {
+                    Console.WriteLine("Previous Hashes not equal");
+                    return false;
+                }
+                //check if hash is solved
+                if (!currentBlock.hash.Substring(0, difficulty).Equals(hashTarget))
+                {
+                    Console.WriteLine("This block hasn't been mined");
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+   
+}
