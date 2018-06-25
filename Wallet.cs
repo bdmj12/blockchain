0d450ccab29f0d48f8f7822f0ee38eb10522d59f
Ji Pattison-Smith <jipattisonsmith@gmail.com>
2018-06-25 19:05:26 +0100

    Fixed build

--- Wallet.cs
+++ Wallet.cs
@@ -1,6 +1,4 @@
-﻿
-
-using System;
+﻿using System;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -13,35 +11,11 @@ namespace Blockchain
         public static string _publicKey;
         private static UnicodeEncoding _encoder = new UnicodeEncoding();
 
-
-
-
         public Wallet()
         {
             var rsa = new RSACryptoServiceProvider();
             _privateKey = rsa.ToXmlString(true);
             _publicKey = rsa.ToXmlString(false);
-
-        }
-
-        public void generatedKeyPair()
-        {
-            try
-            {
-                KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
-                SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-                ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
-                // Initialize the key generator and generate a KeyPair
-                keyGen.initialize(ecSpec, random);   //256 bytes provides an acceptable security level
-                KeyPair keyPair = keyGen.generateKeyPair();
-                // Set the public and private keys from the keyPair
-                privateKey = keyPair.getPrivate();
-                publicKey = keyPair.getPublic();
-            }
-
-
-
-
         }
 
         public string Decrypt(string data)
